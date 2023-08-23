c2e332e8579f2ef45a373943f756eeab102b9fb4
Ivan Klochkov <vany-nov13@yandex.ru>
2023-08-23 14:48:39 +0300

    feat: add solution to reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let numString = Math.abs(n) + '';
+    let answ = '';
+    for (let i = numString.length - 1; i >= 0; i--) {
+        answ += numString[i];
+    }
+    return +answ;
 }
